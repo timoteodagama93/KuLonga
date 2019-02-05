652c1941e1fe415f84b5ad1cf837bba1a13eaa2e
Timóteo Da Gama <timoteodagama93@gmail.com>
2019-02-05 14:44:01 +0100

    An app that helps teachers to control theirs classes. Control avaluation and so on.

---
+++ app/src/main/java/com/netal/kulonga/MainActivity.java
@@ -0,0 +1,24 @@
+package com.netal.kulonga;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
